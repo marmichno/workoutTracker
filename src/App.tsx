193c18fd438235a68515f7637d5bebebc05b6c99
Marcin Michno <mmichno179@gmail.com>
2022-04-10 20:30:01 +0200

    basic router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+// config
+import { paths } from './config/paths';
+// router
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// components
+import { Register } from './routes/register/Register';
+
 const App = () => {
-  return <h1>app</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path={paths.register} element={<Register />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
